6785379cd46c5ae696e48128056d0953168d2e3d
NiharikaSRam <niharika.ram456@gmail.com>
2021-03-29 11:21:03 +0530

    Modified

--- Twenty6.java
+++ Twenty6.java
@@ -2,28 +2,43 @@ import java.util.Scanner;
 
 public class Twenty6 {
 
-    public static void main(String args[])
-       {
-        Scanner sc = new Scanner(System.in);
-        System.out.print("Input a number : ");
-        String nstr = sc.nextLine();
- 
-        int l = nstr.length();  
-        int ctr = 0;  
-        char chr;
- 
-        for(int i=1;i<l;i++)
-        {
-            chr = nstr.charAt(i); 
-            if(chr=='0')
-                ctr++;
-        }
- 
-        char f = nstr.charAt(0); 
- 
-        if(ctr>0 && f!='0')
-            System.out.println("Duck number");
-        else
-            System.out.println("Not a duck number");
-    }
+   // method to check duck number
+   public static boolean isDuck(int number) {
+
+      // loop to repeat the process
+      while(number != 0) {
+
+         // check last digit
+         if(number%10 == 0)
+            return true;
+
+         // remove last digit
+         number /= 10;
+      }
+
+      // else it is not a duck number
+      return false;
+   }
+
+   public static void main(String[] args) {
+
+      // declare variables
+      int number = 0;
+
+      // read the input 
+      Scanner scan =  new Scanner(System.in);
+      System.out.print("Enter an integer number:: ");
+      number = scan.nextInt();
+
+      // check the number is duck number or not
+      if(isDuck(number))
+	 System.out.println(number+" is a"
+		    	+ " duck number");
+      else
+         System.out.println(number+" is not a"
+	      	  	+ " duck number");
+
+      // close Scanner class object
+      scan.close();
+   }
 }
